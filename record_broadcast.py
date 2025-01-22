10b5c4fdd5fd7abe600fc36eaaca75c8471f40a3
White-Tiger-PX <191647395+White-Tiger-PX@users.noreply.github.com>
2025-01-23 02:17:04 +0300

    Размер кольцевого буфера уменьшен

--- record_broadcast.py
+++ record_broadcast.py
@@ -12,13 +12,13 @@ def record_broadcast(recorded_file_path, user_name, app, logger):
             "twitch.tv/" + user_name,
             "best",
             "--ringbuffer-size",
-            "300M",
+            "128M",
             "-o",
             recorded_file_path
         ], creationflags=subprocess.CREATE_NO_WINDOW)
 
         while process.poll() is None:
-            time.sleep(1)  #
+            time.sleep(1)
     except Exception as err:
         logger.error(f"Ошибка во время записи для {user_name}: {err}")
     finally:
