15b7eabc6947ae2fdbea00f9c193ec9dc2b01779
White-Tiger-PX <191647395+White-Tiger-PX@users.noreply.github.com>
2025-02-06 19:10:03 +0300

    Добавлены docstring'и для модуля utils и представленных в нём функций

--- utils.py
+++ utils.py
@@ -1,3 +1,12 @@
+"""
+Модуль общих (базовых) функций.
+
+Краткое описание функций:
+    - create_file_basename: Создает имя файла из списка компонентов.
+    - create_file_path: Формирует полный путь к файлу на основе имени и директории.
+    - get_video_path: Определяет путь для сохранения видео в выбранном хранилище.
+    - get_twitch_user_ids: Получает идентификаторы пользователей Twitch по их логинам или ID.
+"""
 import os
 
 from choose_storage import choose_storage
@@ -5,6 +14,20 @@ from get_twitch_user_id import get_twitch_user_id
 
 
 def create_file_basename(name_components, extension, logger):
+    """
+    Создает имя файла, объединяя компоненты имени и расширение файла.
+
+    Args:
+        name_components (list): Список компонентов, составляющих имя файла.
+        extension (str): Расширение файла (без точки).
+        logger (Logger): Логгер.
+
+    Returns:
+        str: Имя файла.
+
+    Raises:
+        Exception: Генерируется при ошибке создания имени файла.
+    """
     try:
         name_components = [str(item) for item in name_components]
         raw_filename = f"{' - '.join(name_components)}.{extension}"
@@ -20,21 +43,49 @@ def create_file_basename(name_components, extension, logger):
 
 
 def create_file_path(folder_path, name_components, extension, logger):
+    """
+    Создает путь к файлу, используя путь к папке, компоненты имени файла и расширение.
+
+    Args:
+        folder_path (str): Путь к папке, в которой будет создан файл.
+        name_components (list): Список компонентов, составляющих имя файла.
+        extension (str): Расширение файла (без точки).
+        logger (Logger): Логгер.
+
+    Returns:
+        str: Нормализованный полный путь к файлу.
+
+    Raises:
+        Exception: Генерируется при ошибке создания пути к файлу.
+    """
     try:
         basename = create_file_basename(name_components, extension, logger)
         file_path = os.path.join(folder_path, basename)
-
         return os.path.normpath(file_path)
     except Exception as err:
         logger.error(f"Ошибка при создании пути к файлу: {err}")
-
         raise
 
 
 def get_video_path(storages, user_name, name_components, logger):
-    storage_path = choose_storage(storages=storages, logger=logger)
-    folder_path = os.path.join(storage_path, user_name)
+    """
+    Определяет путь для сохранения видео в выбранном хранилище.
 
+    Args:
+        storages (list): Список доступных путей к хранилищам.
+        user_name (str): Имя пользователя Twitch.
+        name_components (list): Список компонентов, составляющих имя файла.
+        logger (Logger): Логгер.
+
+    Returns:
+        str: Путь к файлу видео или None, если не удалось выбрать хранилище.
+    """
+    storage_path = choose_storage(storages=storages, logger=logger)
+
+    if not storage_path:
+        return None
+
+    folder_path = os.path.join(storage_path, user_name)
     os.makedirs(folder_path, exist_ok=True)
 
     file_path = create_file_path(
@@ -48,8 +99,20 @@ def get_video_path(storages, user_name, name_components, logger):
 
 
 def get_twitch_user_ids(client_id, access_token, database_path, user_identifiers, logger):
-    headers = {"Client-ID": client_id, "Authorization": f"Bearer {access_token}"}
+    """
+    Получает список идентификаторов пользователей Twitch по предоставленным идентификаторам или именам.
 
+    Args:
+        client_id (str): Идентификатор клиента Twitch.
+        access_token (str): Токен доступа Twitch.
+        database_path (str): Путь к базе данных для поиска идентификаторов пользователей.
+        user_identifiers (list): Список идентификаторов или имен пользователей Twitch.
+        logger (Logger): Логгер.
+
+    Returns:
+        list: Список уникальных идентификаторов пользователей Twitch.
+    """
+    headers = {"Client-ID": client_id, "Authorization": f"Bearer {access_token}"}
     user_ids = set()
 
     for user_identifier in user_identifiers:
